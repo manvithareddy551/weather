dae1c6b88276a39683641aefdce811d88ac7f234
manvithareddy551 <84329514+manvithareddy551@users.noreply.github.com>
2022-05-19 19:41:07 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <ul className="flex ml-auto w-full font-bold">
+      <li className="text-xs text-gray-800 ml-auto mr-6 border-b-2 border-green-400 cursor-pointer">
+        Weather
+     </li>
+    </ul>
+  );
+}
+
+export default Header;
